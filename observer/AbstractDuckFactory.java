b9f933c95a9d0c6ca9e1f49220125ad88d221ac9
Yunus Sari <ysari@student.tgm.ac.at>
2016-06-04 13:03:16 +0200

    added Jdoc

--- observer/AbstractDuckFactory.java
+++ observer/AbstractDuckFactory.java
@@ -1,5 +1,15 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * Diese abstrakte Klasse AbstractDuck Factory definiert eine Familie von
+ * zusammengehörigen Produkten.
+ * In diesem Fall sind es die Arten von Enten.
+ * 
+ * @author Yunus Sari
+ * @version 04.06.2016
+ * 
+ */
+ 
 public abstract class AbstractDuckFactory {
  
 	public abstract Quackable createMallardDuck();
